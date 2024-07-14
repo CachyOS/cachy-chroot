90ca499863e9f061c6e51bccd3068383e75254f4
Vladislav Nepogodin <nepogodin.vlad@gmail.com>
2024-07-15 01:03:20 +0400

    👷 build: add shell completions

---
+++ build.rs
@@ -0,0 +1,17 @@
+use std::env;
+use std::path::PathBuf;
+
+use clap::CommandFactory;
+use clap_complete::{generate_to, Shell};
+
+include!("src/args.rs");
+
+fn main() {
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    let mut command = Args::command();
+    for shell in [Shell::Bash, Shell::Fish, Shell::Zsh] {
+        generate_to(shell, &mut command, "cachy-chroot", &out_path)
+            .expect("Couldn't generate completion!");
+    }
+}
